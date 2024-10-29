0ae28a3c00cced9f43b1a38c4e7dbf2406bdf309
Franco-M-Enzian <emf0000b@gmail.com>
2024-10-29 23:24:11 +0900

    ハンズオン：ストップウォッチ

--- react-hands-on/src/pages/StopWatch.tsx
+++ react-hands-on/src/pages/StopWatch.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
 
 const StopWatch = () => {
-  return <div>テスト！！！！！</div>
+  const [time, setTime] = useState<number>(0)
+  const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null)
+  const MarginTopBox = styled.div`
+    margin-top: 24%;
+    color: blue;
+  `
+
+const handleStart = () => {
+  if (timerId) return
+  const id: NodeJS.Timeout = setInterval(() => setTime((prevTime) => prevTime + 10), 10)
+  setTimerId(id)
 }
+
+const handleStop = () => {
+  if (timerId) clearInterval(timerId)
+    setTimerId(null)
+}
+
+const handleReset = () => {
+  setTime(0)
+  if (timerId) clearInterval(timerId)
+    setTimerId(null)
+}
+
+  return (
+    <div>
+      <MarginTopBox>{(time / 1000).toFixed(1)} s</MarginTopBox>
+      <div>
+        <button onClick={handleStart}>Start</button>
+      </div>
+      <div>
+        <button onClick={handleStop}>Stop</button>
+      </div>
+      <div>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  )
+}
+
 
 export default StopWatch
