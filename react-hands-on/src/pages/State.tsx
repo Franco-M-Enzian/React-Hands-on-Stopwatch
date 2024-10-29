2f3ef9349e82c5185f22e4599eb337ec58a2b8fc
Franco-M-Enzian <emf0000b@gmail.com>
2024-10-29 23:23:58 +0900

    ハンズオン：カウント

--- react-hands-on/src/pages/State.tsx
+++ react-hands-on/src/pages/State.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
+// アロー関数
 const State = () => {
+  // useState()は、Reactにある標準の関数 初期値を指定
+  // countに対して数値が変更されるsetCount関数を指定
   const [count, setCount] = useState(0)
 
   const MarginTopBox = styled.div`
@@ -10,6 +13,7 @@ const State = () => {
   return (
     <MarginTopBox>
       <p>You clicked {count} times</p>
+      {/* ボタンをクリックすると、setCount関数が実行され、countが1カウントされる */}
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </MarginTopBox>
   )
